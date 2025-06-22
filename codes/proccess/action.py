3fa9f72e1d8a44a74f3e4635d574aa2ac79a83a6
amir aspad <amiraspad1380@gmail.com>
2025-06-22 21:46:44 +0330

    change action file

--- codes/proccess/action.py
+++ codes/proccess/action.py
@@ -3,36 +3,30 @@ from .settings import INPUT_DIR, OUTPUT_DIR
 import os
 
 
-class Encrypt:
-    def __init__(self, password):
+class Base:
+    def __init__(self, password, action):
         self.password = password
+        self.action = action
+        self.from_ = INPUT_DIR if self.action=='enc' else OUTPUT_DIR
+        self.to_ = OUTPUT_DIR if self.action=='enc' else INPUT_DIR
 
     def generate_input_address(self, file):
         # generate input address with only file name
-        return os.path.join(INPUT_DIR, file)
+        return os.path.join(self.from_, file)
     
     def generate_output_address(self, file):
         # generate output address with only file name
-        return os.path.join(OUTPUT_DIR, file)
-
-    def files(self):
-        # find all file in input folder
-        return os.listdir(INPUT_DIR)
+        return os.path.join(self.to_, file)
     
     def read(self, file):
         # read data in file
         with open(self.generate_input_address(file), mode='r') as f:
             return f.read()
         
-    def hash(self, data):
-        # hash data
-        return data
-        
-    def write(self, data, file):
-        # write data in file
-        with open(self.generate_output_address(file), mode='w') as f:
-            f.write(self.hash(data))
-
+    def files(self):
+        # find all file in folder
+        return os.listdir(self.from_)
+    
     def proccess(self):
         try:
             for file in self.files():
@@ -50,26 +44,23 @@ class Encrypt:
         print('[+] proccess down')
 
 
-class Decrypt:
+class Encrypt(Base):
     def __init__(self, password):
-        self.password = password
+        super().__init__(password, 'enc')
 
-    def generate_output_address(self, file):
-        # generate output address with only file name
-        return os.path.join(OUTPUT_DIR, file)
-    
-    def generate_input_address(self, file):
-        # generate input address with only file name
-        return os.path.join(INPUT_DIR, file)
+    def hash(self, data):
+        # hash data
+        return data
+        
+    def write(self, data, file):
+        # write data in file
+        with open(self.generate_output_address(file), mode='w') as f:
+            f.write(self.hash(data))
 
-    def files(self):
-        # find all file in output folder
-        return os.listdir(OUTPUT_DIR)
-    
-    def read(self, file):
-        # read data in file
-        with open(self.generate_output_address(file), mode='r') as f:
-            return f.read()
+
+class Decrypt(Base):
+    def __init__(self, password):
+        super().__init__(password, 'dec')
         
     def unhash(self, data):
         # unhash data
@@ -77,21 +68,6 @@ class Decrypt:
         
     def write(self, data, file):
         # write data in the file
-        with open(self.generate_input_address(file), mode='w') as f:
+        with open(self.generate_output_address(file), mode='w') as f:
             f.write(self.unhash(data))
 
-    def proccess(self):
-        try:
-            for file in self.files():
-                print(f'[+] find {file}')
-                data = self.read(file)
-                print(f'[+] read data from {file}')
-                self.write(data, file)
-                print(f'[+] write data complated')
-        except:
-            print(f'[!] error {file}')
-
-    def run(self):
-        print('[+] proccess started')
-        self.proccess()
-        print('[+] proccess down')
