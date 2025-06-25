4628c451c61691d589d026c1cf5145362bf9c04c
amir aspad <amiraspad1380@gmail.com>
2025-06-25 13:48:49 +0330

    add .env file in to the project

--- codes/proccess/settings.py
+++ codes/proccess/settings.py
@@ -1,6 +1,11 @@
+from dotenv import load_dotenv
 import os
+load_dotenv()
+
 
 BASE_DIR = os.getcwd()
 
 INPUT_DIR = os.path.join(BASE_DIR, 'input')
 OUTPUT_DIR = os.path.join(BASE_DIR, 'output')
+
+PASSWORD = os.getenv('PASSWORD')
